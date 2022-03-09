827954e183bfad07c899f5525400e26c9f37113f
BamunugeDR <drbamunuge99@gmail.com>
2022-03-09 17:20:08 +0530

    renamed2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import AddStudent from "./components/addstudent";
+import AddStudent from "./components/home";
 
 function App() {
   return (
